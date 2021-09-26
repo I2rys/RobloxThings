d9b88b740c41b3800d657bc3463bf3a56dc07293
I2rys <i2rys@protonmail.com>
2021-09-25 21:28:25 -0700

    Create Powerful crash script that can be used for hackers.lua
    A powerful crash script that can be used for hackers than kicking them. Also banning them is powerful way too.

---
+++ Powerful crash script that can be used for hackers.lua
@@ -0,0 +1,51 @@
+local Self = {}
+
+--Functions
+Self.Trash_Name = function()
+	local name = ""	
+	
+	for i = 1, 9999 do
+		if name.len == 0 then
+			name = "true"
+		else
+			name = name..", true"
+		end
+	end
+	
+	return name
+end
+
+Self.Crash = function()
+	game:GetService("RunService").RenderStepped:Connect(function()
+		local Trash = Instance.new("Model")
+		Trash.Name = Self.Trash_Name()
+		Trash.Parent = Character
+		
+		while true do
+			print(true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true)
+			
+			while true do
+				print(true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true)
+			end
+			
+			while true do
+				print(true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true)
+			end
+			
+			while true do
+				print(true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true)
+			end
+			
+			while true do
+				print(true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true)
+			end
+			
+			while true do
+				print(true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true)
+			end
+		end
+	end)
+end
+
+--Main
+Self.Crash()
